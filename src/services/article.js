968c7e1514f1007d7265a81ee297541b9d503582
musegit88 <103637246+musegit88@users.noreply.github.com>
2023-04-27 16:50:29 -0400

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -1,14 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const options = {
-//   method: "GET",
-//   url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
-//   params: { url: "https://time.com/6266679/musk-ai-open-letter/", length: "3" },
-//   headers: {
-//     "X-RapidAPI-Key": "8999ae58cbmsh113cf688875eebcp1a55e0jsn4bacc3edcfd6",
-//     "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
-//   },
-// };
+
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
